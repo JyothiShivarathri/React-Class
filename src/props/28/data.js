efec58ab76032d777748307caedb7b314f8bc717
JyothiShivarathri <sjyothisjyothi4@gmail.com>
2024-04-04 12:57:35 +0530

    space added

--- src/props/28/data.js
+++ src/props/28/data.js
@@ -2,5 +2,6 @@ export const arrayOfArrays=[
     ["Alfreds Futterkiste","Maria Anders","Germany"],
     ["Alfreds Futterkiste","Maria Anders","Germany"],
     ["Alfreds Futterkiste","Maria Anders","Germany"]
-
+    
+    
 ]
